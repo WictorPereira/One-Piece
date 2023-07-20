f1f5397e8e82ab5b2eba3d5255da5c5ba0633108
Joao Wictor <119543998+WictorPereira@users.noreply.github.com>
2023-07-20 01:46:34 -0300

    adicionando slow load

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,11 @@
+    
     //pegando todos elementos da classe pirata
     var piratas = document.querySelectorAll('.pirata')
     //fazendo um for para cada item da lista 
     piratas.forEach(pirata => {
-    //fazendo um evendo de click 
+    //fazendo um evento de click 
     pirata.addEventListener('click', () => {
-    //pegando oq esta dentro da classe selecionando e depois removendo oq tem na clase selecionado  e dps adicionado oq tem na classe  
+    //pegando oq esta dentro da classe selecionando e depois removendo oq tem na classe selecionado  e dps adicionado oq tem na classe  
      var pirataSelecionado = document.querySelector('.selecionado')    
      pirataSelecionado.classList.remove('selecionado')     
      pirata.classList.add('selecionado')
@@ -15,7 +16,8 @@
      var pirataGrande = document.querySelector('.pirata-grande')     
      var idPirata = pirata.attributes.id.value     
      pirataGrande.src = `src/imagens/img-maior/${idPirata}.png` 
-        
+    
+
     // pegando o nome dos piratas e  depois modificando o texto que ira aparecer conforme o idpirata
      var nomePirata = document.getElementById('nome-pirata')
      nomePirata.innerText = idPirata
@@ -23,10 +25,13 @@
     //pegando oq tem dentro do id e guardando em uma variável. logo após isto modificando o texto com innertext com base no data-description que esta la no html.
      var descricaoPirata = document.getElementById('descricao-pirata')
      descricaoPirata.innerText = pirata.getAttribute('data-description')   
-     
-     
-     
+         
+    
    })
 })
+
+
+
+
 
 
